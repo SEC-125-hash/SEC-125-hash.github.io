1b62bab1776325d79f791b8d631a72c136369de0
SEC-125-hash <borissmbanga4@gmail.com>
2025-04-22 18:42:46 +0300

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -1,18 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["full-name"];
-    $email = $_POST["email-address"];
-    $message = $_POST["message"];
 
-    $to = "secglobal23@gmail.com";
-    $subject = "New message from SEC contact form";
-    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
-    $headers = "From: $email";
-
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Message sent successfully.";
-    } else {
-        echo "Failed to send message.";
-    }
-}
-?>
